a98069ba6bf4b4567ddfb135f495ab14b1f68841
Neha Rajpoot <112348114+NehaRajpoot26@users.noreply.github.com>
2023-07-30 12:16:58 +0530

    Create NumberPattern1.cpp

---
+++ Conditionals and Loops/NumberPattern1.cpp
@@ -0,0 +1,31 @@
+// Print the following pattern
+// Pattern for N = 4
+// 1
+// 23
+// 345
+// 4567
+// Sample Input 1 :
+// 3
+// Sample Output 1 :
+// 1
+// 23
+// 345
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,i=1,k,j;
+	cin>>n;
+	while(i<=n)
+	{
+k=i;
+j=1;
+while (j <= i) {
+  cout << k;
+  j++;
+  k++;
+}
+i++;
+cout << endl;
+        }
+}
