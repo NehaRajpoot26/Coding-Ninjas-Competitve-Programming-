6dac4587bce8fcf48fb69a66186cd4728fe8171f
Neha Rajpoot <112348114+NehaRajpoot26@users.noreply.github.com>
2023-07-31 01:07:32 +0530

    Added Question

---
+++ Operators and For Loop/SquareRoot(Integral).cpp
@@ -0,0 +1,16 @@
+// Given a number N, find its square root. You need to find and print only the integral part of square root of N.
+// Sample Input 1 :
+// 10
+// Sample Output 1 :
+// 3
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	// Write your code here
+	int N;
+    cin>>N;
+    int x=sqrt(N);
+    cout<<x;
+}
