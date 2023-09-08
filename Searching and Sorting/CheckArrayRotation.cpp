b3c5610f49e01f003b275c5113183ebf57201c08
NehaRajpoot26 <112348114+NehaRajpoot26@users.noreply.github.com>
2023-09-08 22:48:02 +0530

    Added Question

--- Searching and Sorting/CheckArrayRotation.cpp
+++ Searching and Sorting/CheckArrayRotation.cpp
@@ -1,6 +1,6 @@
 // You have been given an integer array/list(ARR) of size N. It has been sorted(in increasing order) and then rotated by some number 'K' (K is greater than 0) in the right hand direction.
 // Your task is to write a function that returns the value of 'K', that means, the index from which the array/list has been rotated.
-// Sample Input 1:
+// Sample Input 1:-
 // 1
 // 6
 // 5 6 1 2 3 4
